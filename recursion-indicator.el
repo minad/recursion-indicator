ba3cc0765af966e8acc27f6606c8918f442e37e9
Daniel Mendler <mail@daniel-mendler.de>
2022-10-16 12:15:41 +0200

    Version 0.3

--- recursion-indicator.el
+++ recursion-indicator.el
@@ -5,7 +5,7 @@
 ;; Author: Daniel Mendler
 ;; Created: 2020
 ;; License: GPL-3.0-or-later
-;; Version: 0.2
+;; Version: 0.3
 ;; Package-Requires: ((emacs "27.1"))
 ;; Homepage: https://github.com/minad/recursion-indicator
 
