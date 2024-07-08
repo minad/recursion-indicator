e0eccd3b4b7d7bb162767d4c5a028e646437555c
Daniel Mendler <mail@daniel-mendler.de>
2024-07-08 23:19:07 +0200

    Bump Compat dependency

--- recursion-indicator.el
+++ recursion-indicator.el
@@ -7,7 +7,7 @@
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;; Created: 2020
 ;; Version: 1.1
-;; Package-Requires: ((emacs "27.1") (compat "29.1.4.4"))
+;; Package-Requires: ((emacs "27.1") (compat "30"))
 ;; Homepage: https://github.com/minad/recursion-indicator
 ;; Keywords: convenience
 
